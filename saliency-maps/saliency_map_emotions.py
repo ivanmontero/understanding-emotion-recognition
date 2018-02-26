0c5359eac8d9c7f68013194b0792d3d0aecb4c74
ivanmontero <ivanspmontero@gmail.com>
2018-02-26 14:22:22 -0800

    added saliency map creation for emoti-wild model

--- saliency-maps/saliency_map_emotions.py
+++ saliency-maps/saliency_map_emotions.py
@@ -1,4 +1,7 @@
-from keras.applications import VGG16
+#from keras.applications import VGG16
+import sys
+sys.path.insert(0, '../training/emoti-wild/')
+from emoti_wild_keras import KitModelLinear
 import os
 from vis.utils import utils
 from vis.visualization import visualize_saliency, visualize_cam, overlay
@@ -9,27 +12,30 @@ import matplotlib.cm as cm
 import numpy as np
 
 # Build the VGG
-model = VGG16(input_shape=(224, 224, 3),weights='imagenet', include_top=True)
+# model = VGG16(input_shape=(224, 224, 3),weights='imagenet', include_top=True)
+model = KitModelLinear("../training/emoti-wild/emoti-wild-weights.npy")
+model.summary()
 
 # Utility to search for layer index by name. 
 # Alternatively we can specify this as -1 since it corresponds to the last layer.
-layer_idx = utils.find_layer_idx(model, 'predictions')
+layer_idx = utils.find_layer_idx(model, 'prob')
 
 # Swap softmax with linear
 model.layers[layer_idx].activation = activations.linear
 
+
 # Apply modifications
-model.save('temp.h5')
-model = load_model('temp.h5')
-os.remove('temp.h5')
+#model.save('temp.h5')
+#model = load_model('temp.h5')
+#os.remove('temp.h5')
 
 # Load image
-img = utils.load_img('zebra.jpg', target_size=(224, 224))
+img = utils.load_img('elon_musk_zoom.jpg', target_size=(224, 224))
 
 plt.rcParams['figure.figsize'] = (6, 6)
 
 # Zebra
-index = 340
+index = 3
 
 plt.imshow(img)
 plt.show()

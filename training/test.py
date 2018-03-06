50302a3d94296e3c953e4cf755d66af8fc13a94f
ivanmontero <ivanspmontero@gmail.com>
2018-03-06 13:14:17 -0800

    edited test.py to predict images

--- training/test.py
+++ training/test.py
@@ -17,21 +17,35 @@ model = KitModel("./emoti-wild/emoti-wild-weights.npy")
 # Prints a summary of the network
 model.summary()
 
-# Loads in an image of elon musk, which is prediction #427
-img = misc.imread("elon_musk.jpg")
+def predict(img_name):
+    # Loads in an image of elon musk, which is prediction #427
+    # img = misc.imread("elon_musk.jpg")
+    img = misc.imread(img_name)
 
-# Image size before resizing
-print(img.shape)
+    # Image size before resizing
+    print(img.shape)
 
-# Resize image
-img = misc.imresize(img, [224, 224])
+    # Resize image
+    img = misc.imresize(img, [224, 224])
 
-# New image size
-print(img.shape)
+    # New image size
+    print(img.shape)
 
-# Run the image throught the model, and make a prediction
-pred = model.predict(np.array([img]))
-# print(model.predict_classes(img))
+    # Run the image throught the model, and make a prediction
+    pred = model.predict(np.array([img]))
+    # print(model.predict_classes(img))
 
-# Print out the prediction
-print(np.argmax(pred))
+    # Print out the prediction
+    EMOTIONS = {
+        "angry",
+        "disgust",
+        "fear",
+        "joy",
+        "neutral",
+        "sad",
+        "surprise",
+    }
+    return EMOTIONS[np.argmax(pred)]
+
+print(predict("questionable_caleb_1.png"))
+print(predict("questionable_caleb_2.png"))
